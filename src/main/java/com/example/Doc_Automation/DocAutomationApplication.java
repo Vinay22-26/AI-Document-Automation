6c51be672fbbdf1d07c1f82cd7e668aad20a1a30
K VINAY <kamsalivinay522@gmail.com>
2026-03-11 15:53:56 +0530

    Update DocAutomationApplication.java

--- src/main/java/com/example/Doc_Automation/DocAutomationApplication.java
+++ src/main/java/com/example/Doc_Automation/DocAutomationApplication.java
@@ -10,23 +10,12 @@ import java.io.File;
 public class DocAutomationApplication {
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(DocAutomationApplication.class, args);
-<<<<<<< HEAD
-<<<<<<< HEAD
 		System.out.println("---  System started  ---");
-=======
-		System.out.println("---System Active ---");
->>>>>>> 6c245f4e818a2056b1a716baf74edfed472b129c
-=======
-		System.out.println("---  System started  ---");
->>>>>>> e3c77e3 (Update project files)
-
-
 		String[] dirs = {"input", "exports", "archive", "errors"};
 		for (String d : dirs) {
 			File dir = new File(d);
 			if (!dir.exists()) dir.mkdirs();
 		}
-
 		WatchdogController watchdog = context.getBean(WatchdogController.class);
 		try {
 			watchdog.startWatching("input");
